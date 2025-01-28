38cecdf1bc4ff3c69ab17e377980715d856ecd10
Matney <mlmoller21@amarillocollege.com>
2025-01-28 15:26:38 -0600

    done

--- Week1.1/Program.cs
+++ Week1.1/Program.cs
@@ -10,6 +10,25 @@ namespace Week1._1
     {
         static void Main(string[] args)
         {
+            //Asks user for first name 
+            Console.WriteLine("What is your first name?");
+            //Prints out first name
+            string name = Console.ReadLine();
+            //Asks for last name
+            Console.WriteLine("What is your last name?");
+            //Prints out last name
+            string lastName = Console.ReadLine();
+            //Asks for age
+            Console.WriteLine("What is your age?");
+            //Prints out age
+            string age = Console.ReadLine();
+            //Asks for favorite color 
+            Console.WriteLine("What is your favorite color?");
+            //Prints out color
+            string color = Console.ReadLine();
+            //Finally it writes out a sentence with all the user information put and prints it out
+            Console.WriteLine("Hello!" + name + lastName + "You are" + age + "and your favorite color is" + color);
+            Console.ReadLine();
 
         }
     }
